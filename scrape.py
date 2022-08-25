a3d16fce0c3694cd6b2efe66ff16d53948ad564c
Rebecca Peer <rap103@c007dt.canterbury.ac.nz>
2022-08-26 09:12:16 +1200

    create scrape script and test with one consent

---
+++ scrape.py
@@ -0,0 +1,20 @@
+# Script to scrape ECan webportal for freshwater consents
+# Author: R. Peer
+# Contributors: N. Cameron & O. Saunders
+
+#import packages
+import pandas as pd
+import requests
+import urllib.request
+
+#test with one consent number
+consent = 'CRC165168' #groundwater
+
+#load url
+url = 'https://data.ecan.govt.nz:443/data/154/Water/Water%20permit%20use/CSV?ConsentNo='+consent
+r = requests.get(url)
+
+#check response for errors
+#print(r.status_code) #200 = request success
+
+data = r.text
